606b3734f28e006caf3fdb4dd00ec337cdfee42b
Rachel Sharp <rachel@hashicorp.com>
2020-05-12 12:17:27 -0400

    backend correction

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "<YOUR-ORG-NAME>"
+    organization = "hashicorp-rachel"
 
     workspaces {
       name = "learn-terraform-circleci"
