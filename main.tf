786fbb9c541096f7f4bba2ec14f3ffff5783139c
Yordan Halachev <yordan.i.halachev@gmail.com>
2020-10-02 17:31:56 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,74 +4,11 @@ terraform {
       source = "hashicorp/aws"
     }
   }
-  backend "s3" {
-    bucket = "e332661b-b45c-cb95-569e-16d785e16f06-backend"
-    key    = "terraform/webapp/terraform.tfstate"
-    region = "us-east-2"
-  }
+  backend "remote" {
+        organization = "yordanh_free"
+        workspace = "11111111"
+        token=var.token
+    }
 }
 
-provider "aws" {
-  region = var.region
-}
 
-provider "template" {
-}
-
-resource "random_uuid" "randomid" {}
-
-resource "aws_iam_user" "circleci" {
-  name = var.user
-  path = "/system/"
-}
-
-resource "aws_iam_access_key" "circleci" {
-  user = aws_iam_user.circleci.name
-}
-
-data "template_file" "circleci_policy" {
-  template = file("circleci_s3_access.tpl.json")
-  vars = {
-    s3_bucket_arn = aws_s3_bucket.app.arn
-  }
-}
-
-resource "local_file" "circle_credentials" {
-  filename = "tmp/circleci_credentials"
-  content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
-}
-
-resource "aws_iam_user_policy" "circleci" {
-  name   = "AllowCircleCI"
-  user   = aws_iam_user.circleci.name
-  policy = data.template_file.circleci_policy.rendered
-}
-
-resource "aws_s3_bucket" "app" {
-  tags = {
-    Name = "App Bucket"
-  }
-
-  bucket = "${var.app}.${var.label}.${random_uuid.randomid.result}"
-  acl    = "public-read"
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-  force_destroy = true
-
-}
-
-resource "aws_s3_bucket_object" "app" {
-  acl          = "public-read"
-  key          = "index.html"
-  bucket       = aws_s3_bucket.app.id
-  content      = file("./assets/index.html")
-  content_type = "text/html"
-
-}
-
-output "Endpoint" {
-  value = aws_s3_bucket.app.website_endpoint
-}
