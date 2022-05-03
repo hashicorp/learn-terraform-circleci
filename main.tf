5ad90543b55457defdceb64246c2900564e3b74f
ritsok <msokolova13@gmail.com>
2022-05-03 13:24:29 -0400

    WIP

--- main.tf
+++ main.tf
@@ -1,45 +1,14 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.44.0"
-    }
-  }
-  required_version = "> 0.14"
-}
-
 provider "aws" {
   region = var.region
+
+  default_tags {
+    tags = {
+      hashicorp-learn = circleci
+    }
+  }
 }
 
 resource "random_uuid" "randomid" {}
-
-resource "aws_iam_user" "circleci" {
-  name = var.user
-  path = "/system/"
-}
-
-resource "aws_iam_access_key" "circleci" {
-  user = aws_iam_user.circleci.name
-}
-
-data "template_file" "circleci_policy" {
-  template = file("circleci_s3_access.tpl.json")
-  vars = {
-    s3_bucket_arn = aws_s3_bucket.app.arn
-  }
-}
-
-resource "local_file" "circle_credentials" {
-  filename = "tmp/circleci_credentials"
-  content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
-}
-
-resource "aws_iam_user_policy" "circleci" {
-  name   = "AllowCircleCI"
-  user   = aws_iam_user.circleci.name
-  policy = data.template_file.circleci_policy.rendered
-}
 
 resource "aws_s3_bucket" "app" {
   tags = {
@@ -63,9 +32,5 @@ resource "aws_s3_bucket_object" "app" {
   bucket       = aws_s3_bucket.app.id
   content      = file("./assets/index.html")
   content_type = "text/html"
-
 }
 
-output "Endpoint" {
-  value = aws_s3_bucket.app.website_endpoint
-}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "endpoint" {
+  value = aws_s3_bucket.app.website_endpoint
+}
